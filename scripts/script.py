014808dae9166279b030acea0aa971ecb3229138
Jesse Haenen <jessehaenen@gmail.com>
2018-04-17 14:47:09 +0200

    load csv

--- scripts/script.py
+++ scripts/script.py
@@ -1,1 +1,16 @@
 import pandas as pd
+
+
+def load_data(file):
+
+    data = pd.read_csv(file, header=0)
+    df = pd.DataFrame(data)
+
+    return df
+
+
+if __name__ == '__main__':
+
+    df = load_data('../cargo_data/CargoList1.csv')
+
+    print(df)
