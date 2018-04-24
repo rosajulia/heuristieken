8dd8f24b79a22aa43f899bc7d6b34ba2699833b5
juliajelgerhuis <36192695+juliajelgerhuis@users.noreply.github.com>
2018-04-24 12:56:31 +0200

    updated classes.py

--- classes/classes.py
+++ classes/classes.py
@@ -1,5 +1,3 @@
-import csv
-
 class Spaceship():
     """Spaceship object with properties from CSV file."""
     def __init__(self, name, agency, mass, max_weigth, max_volume, base_cost, ftw):
@@ -14,13 +12,6 @@ class Spaceship():
     def __str__(self):
         return "Spaceship name: {}.\nAgency: {}.\nMW: {}.\nMV: {}.\nBC: {}.\nF2W: {}.".format(self.name, self.agency, self.max_weigth, self.max_volume, self.base_cost, self.ftw)
 
-with open("spacecrafts.csv") as f_space:
-    reader = csv.DictReader(f_space)
-    data_space = [r for r in reader]
-
-# remove first line of data_space
-print(data_space)
-
 class Parcel():
     """Parcel object with properties from CSV file."""
     def __init__(self, id, weight, volume, location=0):
@@ -32,10 +23,6 @@ class Parcel():
 
     def __str__(self):
         return "ID: {}.\nWeight: {}.\nVolume: {}.\nRatio: {}.\nLocation: {}.".format(self.id, self.weight, self.volume, self.ratio, self.locaion)
-
-with open("CargoList1.csv") as f_parcel:
-    reader = csv.DictReader(f_parcel)
-    data_parcel = [r for r in reader]
 
 class Inventory():
     """Inventory."""
