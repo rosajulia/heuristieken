2bcdb4607650d57d4e2deedf81af673251c74a56
juliajelgerhuis <36192695+juliajelgerhuis@users.noreply.github.com>
2018-04-24 10:12:27 +0200

    add inventory class

--- classes/classes.py
+++ classes/classes.py
@@ -30,14 +30,25 @@ class Parcel():
         self.ratio = ratio
         self.location = location
 
+    def __str__(self):
+        return "ID: {}.\nWeight: {}.\nVolume: {}.\nRatio: {}.\nLocation: {}.".format(self.id, self.weight, self.volume, self.ratio, self.locaion)
+
 with open("CargoList1.csv") as fParcel:
     reader = csv.DictReader(fParcel)
     dataParcel = [r for r in reader]
 
-class Inventory():
+print(str(dataParcel))
+
+class Inventory(Spaceship):
     """Inventory."""
     def __init__(self, arg):
         self.arg = arg
+        self.name = [[dataSpace[0]["name"], dataSpace[1]["name"], dataSpace[2]["name"], dataSpace[3]["name"]]
+    def __calculatefuelweight__():
+        fuel_weight = (self.mass + self.max_weight) * self.ftw / (1-self.ftw)
+    def __calculate_fuel_costs__():
+        fuel_cost = self.base_cost + roundup(fuel_weight * 100) * 5
+
 
 # class ClassName(object):
 #     """docstring for ."""
