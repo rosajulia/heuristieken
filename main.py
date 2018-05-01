40272c6b4feb7cea21a1f9581a8358ac20e10aba
juliajelgerhuis <36192695+juliajelgerhuis@users.noreply.github.com>
2018-05-01 11:50:51 +0200

    main, classes, algorithm aangepast

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ from scripts import dataloader
 # furute ref: https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python main.py integer")
+        sys.exit(1)
+    
     ship_data = "data/spacecrafts.csv"
     cargo_data = "data/CargoList1.csv"
 
     loaded_data = dataloader.load_data(ship_data, cargo_data)
+    amount_list = []
+    
+    for i in range(int(sys.argv[1])):
+        amount_list.append(randomalgorithm.random_algorithm(loaded_data[0], loaded_data[1]))
+        print("Amount of packages stored: ", end="") 
+        print(amount_list)
 
 if __name__ == "__main__":
    main()
--- scripts/randomalgorithm.py
+++ scripts/randomalgorithm.py
@@ -1,46 +1,32 @@
 #!/usr/bin/env python
-# wat is er random?
-# - random id van pakketje tussen 0 en 100
-#   - kan alleen pseudorandom want als een pakketje al ergens in zit is het 'bezet'
-#   - of mag je nog wisselen later? dan wel random misschien, of random uit de bezette pakketjes
-# - random spaceship om het in te stoppen
-#   - bij max bereikt meteen niet meer suggereren?
-#   - of bij max bereik nog mogen wisselen
-# - random factor in de berekening naar een distribution
-# - random combi van 4*21 of iets daaromheen
-# constraints:
-# - maximaal volume en gewicht
-# - loose of strict?
-# optimaliseren:
-# - combi van pakketjes die dichtst bij de max komt
-
 import random
 
 # create 4 random solutions
 def random_algorithm(dict_space, dict_parcel):
 
-    # for i in range(6):
     ship_counter = 0
     parcel_amount = 0
 
-    # continue adding parcels until reach of maximum payload mass or volume
-    # kan zijn dat de manier van indexeren hier anders moet want nu is het niet subscriptable volgens python
-    while (dict_space[ship_counter].current_weight <= dict_space[ship_counter].max_weight and dict_space[ship_counter].current_volume <= dict_space[ship_counter].max_volume):
+    # start weight and volume at zero
+    for element in dict_space:
+        element.current_weight = 0
+        element.current_volume = 0
 
-        # choose random parcel id to add to ship
-        # check whether 99 or 100 (also depends on id numbers in cargo list)
+    # add parcel till limits are reached
+    while (dict_space[ship_counter].current_weight <= dict_space[ship_counter].max_weight and 
+                dict_space[ship_counter].current_volume <= dict_space[ship_counter].max_volume):
+
+        # pick random parcel
         add_ID = random.randint(1, 100)
 
-        # change location of parcel to correct ship
-        # -1 zodat je de goeie uit de lijst pakt
+        # add parcel to ship
         dict_parcel[add_ID - 1].location = ship_counter
-        # OR array/list of parcels in 4 ships
 
-        # update spaceships current mass and volume
+        # update spaceships current weight and volume
         dict_space[ship_counter].current_weight += dict_parcel[add_ID - 1].weight
         dict_space[ship_counter].current_volume += dict_parcel[add_ID - 1].volume
 
-        # keep track of amount of parcels in current solution
+        # count parcels per solution
         parcel_amount += 1
 
         print("Spaceship: ", end="")
@@ -52,17 +38,14 @@ def random_algorithm(dict_space, dict_parcel):
         print("parcel amount: ", end="")
         print(parcel_amount)
 
-        # itirate over ship to add parcel to
+        # iterate over ship to add parcel to
+        # if ship_counter % 3 != 0:
+        #     ship_counter += 1
+        # else:
+        #     ship_counter = 0
         if (ship_counter == 3):
             ship_counter = 0
         else:
             ship_counter += 1
 
     return parcel_amount
-
-    # display locations of loaded parcels
-    # print({"parcel_amount" : parcel_amount})
-    # print("locations:")
-    # # for parcel in parcels:
-    #     if (parcel.location != 0):
-    #         print({parcel.id : parcel.location})
