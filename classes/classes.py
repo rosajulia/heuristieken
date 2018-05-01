8c425742c3f021d0f19affb546b6c25c29b8e060
juliajelgerhuis <36192695+juliajelgerhuis@users.noreply.github.com>
2018-05-01 12:42:04 +0200

    classes mooier gemaakt

--- classes/classes.py
+++ classes/classes.py
@@ -1,7 +1,6 @@
 class Spaceship():
     """Spaceship object with properties from CSV file."""
     def __init__(self, id, mass, max_weight, max_volume, base_cost, ftw, current_weight=0, current_volume=0):
-        # hier heb ik ints en floats van gemaakt zodat je er straks mee kunt rekenen (waren strings maar misschien straks niet meer nodig)
         self.id = int(id)
         self.mass = int(mass)
         self.max_weight = float(max_weight)
@@ -14,22 +13,9 @@ class Spaceship():
     def tryShip(self):
         return {"id": self.id, "mass": self.mass, "max_weight": self.max_weight, "max_volume": self.max_volume, "base_cost": self.base_cost, "ftw": self.ftw, "current_weight": self.current_weight, "current_volume": self.current_volume}
 
-    # heb dit in repr veranderd (was str) en wat punten naar komma's en een vraagteken (om eventueel te splitten)
-    # def __repr__(self):
-    #     return "Spaceship name: {}\nMW: {},\nMV: {},\nBC: {},\nF2W: {}?".format(self.id, self.max_weight, self.max_volume, self.base_cost, self.ftw)
-
-    # heb dit in repr veranderd (was str) en wat punten naar komma's en een vraagteken (om eventueel te splitten)
-    # def __repr__(self):
-    #     return "Spaceship name: {}\nMW: {},\nMV: {},\nBC: {},\nF2W: {}?".format(self.id, self.max_weight, self.max_volume, self.base_cost, self.ftw)
-    #
-    # # hieronder is poging tot subscriptable object maken, maar daarmee krijg je als je print de locaties van de objecten
-    # def output(self):
-    #     return str({'id': self.id, 'MW': self.max_weight, 'MV': self.max_volume, 'BC': self.base_cost, 'F2W': self.ftw })
-
 class Parcel():
     """Parcel object with properties from CSV file."""
     def __init__(self, id, weight, volume, location=0):
-        # hier heb ik ints en floats van gemaakt zodat je er straks mee kunt rekenen (waren strings maar misschien straks niet meer nodig)
         self.id = int(id)
         self.weight = float(weight)
         self.volume = float(volume)
@@ -39,17 +25,8 @@ class Parcel():
     def tryParcel(self):
         return {"id": self.id, "weight": self.weight, "volume": self.volume, "ratio": self.ratio, "location": self.location}
 
-    # heb dit in repr veranderd (was str) en wat punten naar komma's en een vraagteken (om eventueel te splitten)
-    # def __repr__(self):
-    #     return "ID: {},\nWeight: {},\nVolume: {},\nRatio: {},\nLocation: {}?".format(self.id, self.weight, self.volume, self.ratio, self.location)
-
-    # hieronder is poging tot subscriptable object maken, maar daarmee krijg je als je print de locaties van de objecten
-    # def output(self):
-    #     return str({'ID': self.id, 'Weight': self.weight, 'Volume': self.volume, 'Ratio': self.ratio, 'Location': self.location})
-
 class Inventory():
     """Inventory."""
-    # volgens mij mag ftw hier uit want dat is voor ieder ship anders en haal je uit dict space(?)
     def __init__(self, id_space, dict_space, dict_parcel, ftw):
         self.id_space = id_space
         self.dict_space = dict_space
