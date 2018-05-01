b5633d659e1a69b7ae314f3c4cbdb5e701dca897
Jesse Haenen <jessehaenen@gmail.com>
2018-05-01 14:23:00 +0200

    graph bins

--- scripts/graph.py
+++ scripts/graph.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 def barchart(results):
     
     # plot solutions in histogram
-    plt.hist(results, range=(0, 100), align='mid')
+    plt.hist(results, bins=list(range(100)), range=(0, 100), align='mid')
     plt.title = ("Solutions")
     plt.xlabel = ("Frequency")
     plt.ylabel = ("Amount of parcels")
