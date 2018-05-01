c33525def69338cad216f9efb0a74453f5ce3aaa
juliajelgerhuis <36192695+juliajelgerhuis@users.noreply.github.com>
2018-05-01 14:25:19 +0200

    fix

--- main.py
+++ main.py
@@ -19,23 +19,11 @@ def main():
 
     for i in range(int(sys.argv[1])):
         amount_list.append(randomalgorithm.random_algorithm(loaded_data[0], loaded_data[1]))
-<<<<<<< HEAD
-
-    
-    # plot solutions in histogram
-    plt.hist(amount_list)
-    plt.title = ("Solutions")
-    plt.xlabel = ("Frequency")
-    plt.ylabel = ("Amount of parcels")
-    plt.show()
-
-=======
     
     print(max(amount_list))
 
     # plot solutions in histogram
     graph.barchart(amount_list)
->>>>>>> b5633d659e1a69b7ae314f3c4cbdb5e701dca897
 
 if __name__ == "__main__":
     main()
