49b95f770a22683013a155a16dd1ca35a524e11e
Jesse Haenen <jessehaenen@gmail.com>
2018-05-01 15:12:30 +0200

    time logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import datetime
 from scripts import randomalgorithm, dataloader, graph
 
 # necessary for this script: pip install matplotlib
@@ -17,8 +18,12 @@ def main():
     loaded_data = dataloader.load_data(ship_data, cargo_data)
     amount_list = []
 
+    print('{}: Start random algorithm...'.format(datetime.datetime.now().strftime("%H:%M:%S")))
+    
     for i in range(int(sys.argv[1])):
         amount_list.append(randomalgorithm.random_algorithm(loaded_data[0], loaded_data[1]))
+    
+    print('{}: Finished running {} times.'.format(datetime.datetime.now().strftime("%H:%M:%S"), sys.argv[1]))
     
     print(max(amount_list))
 
