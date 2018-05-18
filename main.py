1dbc4d290b884bce44850aadab3860bd4f284ffd
fietsboekstoelgit config --global user.name fietsboekstoelgit commit -m first <fietsboekstoel>
2018-05-18 15:05:50 +0200

    voor werkcollege

--- main.py
+++ main.py
@@ -2,24 +2,14 @@
 
 import sys
 import datetime
-from algorithms import randomalgorithm, greedyratio
+from algorithms import randomalgorithm
+from scripts import graph, greedyratio
 from data import dataloader
-from visualisation import graph
 from classes import classes
-
-from flask import Flask, render_template, Response
-import time
 
 # necessary for this script: pip install matplotlib
 # furute ref: https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 
-app = Flask(__name__, template_folder="visualisation")
-
-@app.route("/")
-def index():
-    return render_template("visual.html")
-
-@app.route('/progress')
 def main():
     if len(sys.argv) != 2:
         print("Usage: python main.py integer")
@@ -27,87 +17,51 @@ def main():
 
     # load data
     ship_data = "data/spacecrafts.csv"
-
-    # hieronder de 1 aanpassen naar bijbehorend command-line argument
     cargo_data = "data/CargoList1.csv"
-
-    # dataloader aanroepen met 4/oneindig en wel/geen constraints
     inventory = dataloader.load_data(ship_data, cargo_data)
 
-    # create list for solutions
+    print('{}: Start random algorithm...'.format(datetime.datetime.now().strftime("%H:%M:%S")))
+
+    # make list of solutions
     solutions = []
     costs = 0
 
-    # hier ofwel random, ofwel greedy, ofwel hillclimber aanroepen met aantal keer
+    for i in range(int(sys.argv[1])):
+        # result = randomalgorithm.random_algorithm(inventory.dict_space, inventory.dict_parcel)
+        result = randomalgorithm.random_algorithm(inventory)
+        # calculate total costs
+        weight = [result.get("weight1"), result.get("weight2"), result.get("weight3"), result.get("weight4")]
 
-    # start greedy algorithm (command-line argument nog aanpassen!)
-    # deze for loop moet in het algoritme niet erbuiten (vooral vanwege hillclimber)
-    for _ in range(int(sys.argv[1])):
-        print('{}: Start greedy algorithm...'.format(datetime.datetime.now().strftime("%H:%M:%S")))
+        for ship in range(len(inventory.dict_space)):
+            costs += inventory.calculate_fuel_costs(ship, weight[ship])
 
-        # de generator slaat de geyielde waardes op
-        generator = greedyratio.greedy_ratio(inventory)
+        # append solutions to list
+        solutions.append(classes.Inventory(inventory.dict_space, inventory.dict_parcel, \
+                            i, result.get("parcel_amount"), costs))
 
-        for result in generator:
-            try:
-                result = next(generator)
-                print(result[0])
-            except:
-                print("error")
-                break
+    # calculate best solution parcel-wise
+    best_parcel = (max([solution.parcel_amount for solution in solutions]))
+    for solution in solutions:
+        if solution.parcel_amount == best_parcel:
+            best_parcel_costs = solution.total_costs
 
-        exit(1)
-
-        # result = next(generator)
-        #
-        # # calculate total costs
-        # result_weight = [result.get("weight1"), result.get("weight2"), result.get("weight3"), result.get("weight4")]
-        #
-        # for ship in range(len(inventory.dict_space)):
-        #     costs += inventory.calculate_fuel_costs(ship, result_weight[ship])
-        #
-        # # append solutions to list
-        # solutions.append(classes.Inventory(inventory.dict_space, inventory.dict_parcel, \
-        #                     i, result.get("parcel_amount"), costs))
+    # calculate best solution cost-wise
+    best_costs = (min([solution.total_costs for solution in solutions]))
+    for solution in solutions:
+        if solution.total_costs == best_costs:
+            best_costs_parcels = solution.parcel_amount
 
 
+    print('{}: Finished running {} times.'.format(datetime.datetime.now().strftime("%H:%M:%S"), sys.argv[1]))
 
-        # initialize generator for visualisation
-        def generate():
-            weight = 0
+    print("Maximum amount of parcels in ship: {}".format(best_parcel))
+    print("Corresponding costs: {}". format(best_parcel_costs))
 
-            while weight <= 100:
-                # hou hier de geyielde data vast zodat je het naar html kan sturen
-                yield "data:" + str(weight) + "\n\n"
-                # reken de geyielde data om naar percentages (alleen 2000 gebruikt omdat het nog alleen werkt met het eerste schip)
-                weight = weight + next(generator)[0] / 2000 * 100
-            print(next(generator)[1])
-        # stuur de response van functie generate door naar html, waar javascript er shit mee gaat doen
-        return Response(generate(), mimetype= 'text/event-stream')
+    print("The lowest costs of all solutions: {}".format(best_costs))
+    print("Corresponding amount of parcels moved: {}".format(best_costs_parcels))
 
-    # # calculate best solution parcel-wise
-    # best_parcel = (max([solution.parcel_amount for solution in solutions]))
-    # for solution in solutions:
-    #     if solution.parcel_amount == best_parcel:
-    #         best_parcel_costs = solution.total_costs
-    #
-    # # calculate best solution cost-wise
-    # best_costs = (min([solution.total_costs for solution in solutions]))
-    # for solution in solutions:
-    #     if solution.total_costs == best_costs:
-    #         best_costs_parcels = solution.parcel_amount
-    #
-    #
-    # print('{}: Finished running {} times.'.format(datetime.datetime.now().strftime("%H:%M:%S"), sys.argv[1]))
-    #
-    # print("Maximum amount of parcels in ship: {}".format(best_parcel))
-    # print("Corresponding costs: {}". format(best_parcel_costs))
-    #
-    # print("The lowest costs of all solutions: {}".format(best_costs))
-    # print("Corresponding amount of parcels moved: {}".format(best_costs_parcels))
-    #
-    # # plot solutions in histogram
-    # graph.barchart([solution.parcel_amount for solution in solutions])
+    # plot solutions in histogram
+    graph.barchart([solution.parcel_amount for solution in solutions])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    main()
