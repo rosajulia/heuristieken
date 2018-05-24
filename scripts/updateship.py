2cdf657129a0de61ce9021db23136aee2291b49a
fietsboekstoelgit config --global user.name fietsboekstoelgit commit -m first <fietsboekstoel>
2018-05-24 11:28:22 +0200

    jeej

--- scripts/updateship.py
+++ scripts/updateship.py
@@ -1,17 +1,20 @@
 def updateship(ship, parcel):
-
     """
-    Returns an updated ship with adjusted weight and volume.
+    This script updates the attributes of a ship when a parcel is stored in it.
+
+    Returns an object of type Spaceship.
 
     Take two arguments:
 
-        ship: Must contain ship object with loaded attributes.
+        ship: Must contain object of type Spaceship.
 
-        parcel: Must contain parcel object with loaded attributes.
+        parcel: Must contain object of type Parcel.
     """
 
+    # store a copy of the current ship
     updated_ship = ship
 
+    # update the properties of the current ship
     updated_ship.current_weight = updated_ship.current_weight - parcel.weight
     updated_ship.current_volume = updated_ship.current_volume - parcel.volume
 
