0263c1eaf66dffe6550406c1e369dfac446cdaaa
Jesse Haenen <jessehaenen@gmail.com>
2018-05-25 11:58:10 +0200

    merge writeresults

--- visualisation/writeresults.py
+++ visualisation/writeresults.py
@@ -1,39 +1,16 @@
-# inventory
-# > 
-# sol id autoincrement
-# algoritme type + parameters
-# aantal pakketjes
-# kosten
-# percentage van upper bound
-# percentage gevuld gewicht
-# percentage gevuld gewicht
-# pakketjes in ship
-
 import csv
 from classes import classes
 from statistics import mean
 import upperbounds as bounds
 
-def writeresults(solutions, algorithm, cargolist_no, constraint):
+def writeresults(solutions, cargo, ships, constr, algo, b_var, hc, hc_n, iter_n):
+# def writeresults(solutions, args):
+
     """
     This script writes results to a csv file. A result is defined as a fleet 
-    with a parcel distribution. Writes the following attributes:
-        - (CSV-specific) solution id
-        - Algorithm type
-        - Hill Climber
-        - Simulated Annealing
-        - Diplomatic constraint
-        - Number of iterations
-        - Cargolist
-        - Number of packages
-        - Total costs
-        - Total costs (as % of upper bound)
-        - Percentage mass filled
-        - Percentage volume filled
-        - Parcel distribution
-
-    Takes one argument:
-        inventory: Object of type inventory.
+    with a parcel distribution. It logs all command line arguments that are used
+    for that run of the program and its results.
+    Takes eight arguments:
     """
 
     infile = "data/results.csv"
@@ -44,7 +21,7 @@ def writeresults(solutions, algorithm, cargolist_no, constraint):
         writer = csv.writer(outfile, delimiter=',')
 
         # write header with column names
-        writer.writerow("sol_id", "algo", "hc", "sa", "constr", "n_iter", "cargolist", "n_packages", "total_costs", "total_costs_p", "avg_mass_p", "avg_vol_p", "distr")
+        writer.writerow("sol_id", "cargolist", "ships", "constr", "algo", "b_var", "hc", "hc_n", "iter_n", "packages_n", "total_costs", "total_costs_pct", "mass_pct", "volume_pct")
 
         # for every row in the solution
         for solution in solutions:
@@ -52,27 +29,17 @@ def writeresults(solutions, algorithm, cargolist_no, constraint):
             # check if the solution is of type inventory
             if type(solution) == inventory_type:
 
-                # save fleet in the solution
+                # store fleet of the solution
                 fleet = solution.dict_space
-                
-                # hold variables to be written to csv
-                sol_id = id_counter
-                # ALGORITHM
-                # CARGOLIST
-                n_packages = solution.parcel_amount
+
+                # result variables
+                packages_n = solution.parcel_amount
                 total_costs = solution.calculate_costs()
                 total_costs_pct = total_costs / bounds.calc_upperbound()
-                mass_filled_pct = mean([ship.current_weight for ship in fleet])
-                volume_filled_pct = mean([ship.current_volume for ship in fleet])
+                mass_pct = mean([ship.current_weight for ship in fleet])
+                volume_pct = mean([ship.current_volume for ship in fleet])
 
                 # write evey row in the csv
-                writer.writerow(sol_id, n_packages, total_costs, total_costs_pct, mass_filled_pct, volume_filled_pct)
+                writer.writerow(id_counter, cargo, ships, constr, algo, b_var, hc, hc_n, iter_n, packages_n, total_costs, total_costs_pct, mass_pct, volume_pct)
 
                 id_counter += 1
-
-
-
-
-
-
-
